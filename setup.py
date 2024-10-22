b381301708b3103317ccd5107d4356e92d254f93
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:38:22 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,15 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
+from exodide import build
+
+# omit
 
 ext_modules = [Extension("image_processing", ["image_processing/image_processing.pyx"],extra_compile_args=["-Wno-implicit-function-declaration","-Wno-int-conversion"])]
 
 setup(
     name="image_processing",
+    # omit
+    cmdclass=build.cmdclass(), # {'build': build.build, 'build_ext': build.build_ext}
     ext_modules=cythonize(ext_modules)
 )

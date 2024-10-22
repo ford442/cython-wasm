a14365c472442d516a57040db1683b949b6a26ed
Noah Cohn <noahc42@gmail.com>
2024-10-21 17:06:37 -0700

    Update cytho_pyo.pyx

--- cytho_pyo.pyx
+++ cytho_pyo.pyx
@@ -1,24 +1,20 @@
-
 import numpy as np
-cimport numpy as np 
+cimport numpy as np
+from skimage import exposure, transform
+from skimage.filters import unsharp_mask
+import cv2
 
 cpdef np.ndarray[np.uint8_t, ndim=2] process_tile(np.ndarray[np.uint8_t, ndim=2] tile):
     cdef int rows, cols
     cdef np.ndarray[np.float32_t, ndim=2] input_pts, output_pts
     cdef np.ndarray[np.float32_t, ndim=2] M 
-    cdef np.ndarray[np.float32_t, ndim=2] dst
-    cdef np.ndarray[np.float32_t, ndim=2] img_eq
-    cdef np.ndarray[np.float32_t, ndim=2] resize4x
-    cdef np.ndarray[np.float32_t, ndim=2] result_1
-    cdef np.ndarray[np.float32_t, ndim=2] processed_tile
-    M = cv2.getAffineTransform(input_pts , output_pts)
+    rows, cols = tile.shape[:2]
+    input_pts = np.float32([[0,0], [cols-1,0], [0,rows-1]])
+    output_pts = np.float32([[cols-1,0], [0,0], [cols-1,rows-1]])
+    M = cv2.getAffineTransform(input_pts, output_pts)
     dst = cv2.warpAffine(tile, M, (cols,rows))
-    img_eq: np.ndarray = exposure.equalize_hist(dst)
-    del dst
-    resize4x: np.ndarray = transform.rescale(tile, 2)
-    del tile
-    result_1: np.ndarray = unsharp_mask(resize4x, radius=1, amount=1)
-    del resize4x
-    processed_tile: np.ndarray = transform.pyramid_reduce(result_1,2)
-    del result_1
+    img_eq = exposure.equalize_hist(dst)
+    resize4x = transform.rescale(tile, 2)
+    result_1 = unsharp_mask(resize4x, radius=1, amount=1)
+    processed_tile = transform.pyramid_reduce(result_1, 2)
     return processed_tile

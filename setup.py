0de3c57c894410464f3a8c99edb2580fea64d49d
Noah Cohn <noahc42@gmail.com>
2024-10-21 22:39:42 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,22 @@
 from setuptools import setup
 from Cython.Build import cythonize
-from exodide import build
+
+# Define your custom compile arguments
+compile_args = ["-O3",-Wno-implicit-function-declaration","-Wno-int-conversion"]
 
 setup(
-    name='image_processing',
-    version='0.1.0',
-    install_requires=[
-        'scikit-image',
-        'opencv-python'
-    ],
-    ext_modules = cythonize("image_processing/image_processing.pyx",compiler_directives={'language_level' : "3"}),
-    zip_safe=False,
-    cmdclass=build.cmdclass(), # {'build': build.build, 'build_ext': build.build_ext}
-    # Specify the Python version here
-    python_requires='==3.12',
+    name="image_processing",
+    version="0.1",
+    description="A Pyodide-compatible package",
+    author="1inkus",
+    author_email="admin@1ink.us",
+    url="https://github.com/ford442/cython-wasm",
+    packages=["image_processing"],
+    ext_modules=cythonize(
+        "image_processing/image_processing.pyx",
+        compiler_directives={"language_level": "3"},
+        build_dir="build",
+        annotate=True,
+        compile_args=compile_args  # Add your custom compile arguments here
+    ),
 )

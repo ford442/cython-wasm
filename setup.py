849dcf26691f619a84434bb47a029ddd5e01bacf
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:15:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-ext_modules = [Extension("image_processing", ["image_processing/image_processing.pyx"])]
+ext_modules = [Extension("image_processing", ["image_processing/image_processing.pyx"],include_dirs=[np.get_include()],extra_compile_args=["-Wno-implicit-function-declaration"])]
 
 setup(
     name="image_processing",

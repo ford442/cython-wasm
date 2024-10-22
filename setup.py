fbdc10dba87499908e6fc095808f443648953664
Noah Cohn <noahc42@gmail.com>
2024-10-21 17:09:17 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,15 @@
 from setuptools import setup
+from exodide import build
 from Cython.Build import cythonize
 
 setup(
+    name='image_processing',
+    version='0.1.0',
+    install_requires=[
+        'scikit-image',
+        'opencv-python'
+    ],
     ext_modules = cythonize("cytho_pyo.pyx",compiler_directives={'language_level' : "3"}),
+    cmdclass=build.cmdclass(),
+
 )

ebd22701c89c7b2d8c7798036cee5329335dc21a
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:12:47 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,12 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
-from exodide import build
-import numpy as np
 
-# omit
-
-ext_modules = [
-    Extension("image_processing", ["image_processing/image_processing.pyx"],include_dirs=[np.get_include()],extra_compile_args=["-Wno-implicit-function-declaration"]
-)
-]
+ext_modules = [Extension("image_processing", ["image_processing.pyx"])]
 
 setup(
     name="image_processing",
-    ext_modules=cythonize(ext_modules),
-    cmdclass=build.cmdclass(), # {'build': build.build, 'build_ext': build.build_ext}
+    ext_modules=cythonize(ext_modules)
 )
+
+# In your Python code
+from image_processing import process_tile

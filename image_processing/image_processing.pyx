76783604d1b19ecb9b767a22f99bc3637887e0c6
Noah Cohn <noahc42@gmail.com>
2024-10-21 19:55:50 -0700

    Update image_processing.pyx

--- image_processing/image_processing.pyx
+++ image_processing/image_processing.pyx
@@ -12,9 +12,9 @@ cpdef np.ndarray[np.uint8_t, ndim=2] process_tile(np.ndarray[np.uint8_t, ndim=2]
     rows, cols = tile.shape[:2]
     input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
     output_pts = np.float32([[cols-1,0], [0,0], [cols-1,rows-1]])
-    M = cv2.getAffineTransform(input_pts, output_pts)
-    dst = cv2.warpAffine(tile, M, (cols,rows))
-    img_eq = exposure.equalize_hist(dst)
+  #  M = cv2.getAffineTransform(input_pts, output_pts)
+  #  dst = cv2.warpAffine(tile, M, (cols,rows))
+  #  img_eq = exposure.equalize_hist(dst)
  #   resize4x = transform.rescale(tile, 2)
  #   result_1 = unsharp_mask(resize4x, radius=1, amount=1)
  #   processed_tile = transform.pyramid_reduce(result_1, 2)

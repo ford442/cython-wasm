25cec47c238efb772b291f674b848d3de16e2f24
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:42:31 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,14 @@ from exodide import build
 ext_modules = [Extension("image_processing", ["image_processing/image_processing.pyx"],extra_compile_args=["-Wno-implicit-function-declaration","-Wno-int-conversion"])]
 
 setup(
-    name="image_processing",
+    name='image_processing',
+    version='0.1.0',
+    install_requires=[
+        'scikit-image',
+        'opencv-python',
+        'numpy'
+    ],
     # omit
     cmdclass=build.cmdclass(), # {'build': build.build, 'build_ext': build.build_ext}
-    ext_modules=cythonize(ext_modules)
+    ext_modules = cythonize("image_processing/image_processing.pyx",compiler_directives={'language_level' : "3"}),
 )

75693f3ad744d9b20fddcea74709f5ef17144f2e
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:29:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
-from exodide.build import build  # Import the command
+import exodide
 
 ext_modules = [Extension("image_processing", ["image_processing/image_processing.pyx"],extra_compile_args=["-Wno-implicit-function-declaration","-Wno-int-conversion"])]
 
 setup(
     name="image_processing",
-    cmdclass={"bdist_wheel": build(python_version="3.12")},  # Specify Python version
+    cmdclass={"bdist_wheel": exodide_wheel(python_version="3.12")},  # Specify Python version
     ext_modules=cythonize(ext_modules)
 )
-

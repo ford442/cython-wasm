391b60da46c00ccdcab6414d1a8b863787ef9b91
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:01:41 -0700

    Update image_processing.pyx

--- image_processing/image_processing.pyx
+++ image_processing/image_processing.pyx
@@ -5,11 +5,11 @@ from skimage import exposure, transform
 from skimage.filters import unsharp_mask
 import cv2
 
-cpdef np.ndarray[np.uint8_t, ndim=2] process_tile(np.ndarray[np.uint8_t, ndim=2] tile):
-  #  cdef int rows, cols
-  #  cdef np.ndarray[np.float32_t, ndim=2] input_pts, output_pts
-  #  cdef np.ndarray[np.float32_t, ndim=2] M 
-  #  rows, cols = tile.shape[:2]
+def np.ndarray[np.uint8_t, ndim=2] process_tile(np.ndarray[np.uint8_t, ndim=2] tile):
+    cdef int rows, cols
+    cdef np.ndarray[np.float32_t, ndim=2] input_pts, output_pts
+    cdef np.ndarray[np.float32_t, ndim=2] M 
+    rows, cols = tile.shape[:2]
   #  input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
   #  output_pts = np.float32([[cols-1,0], [0,0], [cols-1,rows-1]])
   #  M = cv2.getAffineTransform(input_pts, output_pts)

06f8ca19313b8067a6a51edef8ac46b51e6d9545
Noah Cohn <noahc42@gmail.com>
2024-10-21 20:27:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,10 @@ ext_modules = [Extension("image_processing", ["image_processing/image_processing
 
 setup(
     name="image_processing",
+    cmdclass={"bdist_wheel": exodide_wheel(python_version="3.12")},  # Specify Python version
     ext_modules=cythonize(ext_modules)
 )
 
 # In your Python code
 from image_processing import process_tile
+e

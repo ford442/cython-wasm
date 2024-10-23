7b08210c073b34170cf77833e7abf46a4699a21d
Noah Cohn <noahc42@gmail.com>
2024-10-23 16:50:17 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -5,9 +5,9 @@ from skimage import exposure, transform
 from skimage.filters import unsharp_mask
 import cv2
 
-cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3] tile):
+cpdef np.ndarray[np.uint8_t, ndim=2] process_tile(np.ndarray[np.uint8_t, ndim=2] tile):
     cdef int rows, cols
-    cdef np.ndarray[np.float32_t, ndim=3] input_pts, output_pts, M, dst, img_eq, resize4x, result_1, processed_tile
+    cdef np.ndarray[np.float32_t, ndim=2] input_pts, output_pts, M, dst, img_eq, resize4x, result_1, processed_tile
     rows, cols = tile.shape[:2]
     input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
     output_pts = np.float32([[cols-1,0], [0,0], [cols-1,rows-1]], dtype=np.float32)

ddd446dcf2c69eb3d83a57a92a4c22ef3926ec28
Noah Cohn <noahc42@gmail.com>
2024-10-23 22:28:00 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -25,5 +25,5 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     del dst
     result_1 = unsharp_mask(resize4x, radius=1, amount=1)
     del resize4x
-    processed_tile = transform.pyramid_reduce(result_1, 2)
+    processed_tile = transform.pyramid_reduce(result_1, 2).astype(np.uint8)
     return processed_tile

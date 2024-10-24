5656cd66ce39004b3c0d1d6043b4961f6f205e26
Noah Cohn <noahc42@gmail.com>
2024-10-23 19:30:15 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -15,7 +15,7 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     cdef np.ndarray[np.float64_t, ndim=2] M
     cdef np.ndarray[np.float32_t, ndim=2] N
     cdef np.ndarray[np.float64_t, ndim=3] dst, tile2, processed_tile
-    cdef np.ndarray[np.float64_t, ndim=6] resize4x, result_1
+    cdef np.ndarray[np.float64_t, ndim=3] resize4x, result_1
     rows, cols = tile.shape[:2]
     input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
     output_pts = np.array([[cols-1,0], [0,0], [cols-1,rows-1]], dtype=np.float32)
@@ -26,6 +26,8 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     dst = cv2.warpAffine(tile2, N, (cols,rows))
     del tile2
     resize4x = transform.rescale(dst, 2)
+    resize4x64=
+    
     del dst
     result_1 = unsharp_mask(resize4x, radius=1, amount=1)
     del resize4x

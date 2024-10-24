c0dc5e10c75bf3682400ad0227c84f32138fa293
Noah Cohn <noahc42@gmail.com>
2024-10-23 22:32:56 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -11,19 +11,19 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     cdef np.ndarray[np.float64_t, ndim=2] M
     cdef np.ndarray[np.float32_t, ndim=2] N
     cdef np.ndarray[np.uint8_t, ndim=3] processed_tile
-    cdef np.ndarray[np.float32_t, ndim=3] dst, tile2,resize4x, result_1
+    cdef np.ndarray[np.uint8_t, ndim=3] dst, tile2,resize4x, result_1
     rows, cols = tile.shape[:2]
     input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
     output_pts = np.array([[cols-1,0], [0,0], [cols-1,rows-1]], dtype=np.float32)
     M = cv2.getAffineTransform(input_pts, output_pts)
     N = M.astype(np.float32)
-    tile2 = tile.astype(np.float32)
+    #tile2 = tile.astype(np.float32)
     del tile
-    dst = cv2.warpAffine(tile2, N, (cols,rows))
+    dst = cv2.warpAffine(tile, N, (cols,rows))
     del tile2
     resize4x = transform.rescale(dst, 2)
     del dst
     result_1 = unsharp_mask(resize4x, radius=1, amount=1)
     del resize4x
-    processed_tile = transform.pyramid_reduce(result_1, 2).astype(np.uint8)
+    processed_tile = transform.pyramid_reduce(result_1, 2) #.astype(np.uint8)
     return processed_tile

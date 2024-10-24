50aa8f8455fe1800562e0286967754a80a5148c8
Noah Cohn <noahc42@gmail.com>
2024-10-23 20:50:50 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -33,4 +33,4 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     # result_1 = (result_1 - result_1.min()) / (result_1.max() - result_1.min()) * 255
     #del resize4x
     processed_tile = transform.pyramid_reduce(result_1, 2).astype(np.float32)
-    return processed_tile
+    return tile

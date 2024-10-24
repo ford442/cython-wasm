44aa328c152bbf9eea25523c3ed92cf2767a5a3d
Noah Cohn <noahc42@gmail.com>
2024-10-23 17:45:58 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -16,7 +16,7 @@ cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3]
     output_pts = np.array([[cols-1,0], [0,0], [cols-1,rows-1]], dtype=np.float32)
     M = cv2.getAffineTransform(input_pts, output_pts)
     N = M.astype(np.float32)
-    dst = cv2.warpAffine(tile, M, (cols,rows))
+    dst = cv2.warpAffine(tile, N, (cols,rows))
     img_eq = exposure.equalize_hist(dst)
     resize4x = transform.rescale(tile, 2)
     result_1 = unsharp_mask(resize4x, radius=1, amount=1)

a36e150e0e3029e0597b0ff886303282df0f2bda
Noah Cohn <noahc42@gmail.com>
2024-10-27 02:13:58 -0700

    Update __init__.py

--- image_proc/__init__.py
+++ image_proc/__init__.py
@@ -1,5 +1,7 @@
 from image_proc._processing import process_tile
 from image_proc._processing import process_tile2
 from image_proc._processing import process_tile3
+from image_proc._processing import process_tile4
+from image_proc._processing import process_tile5
 
 __all__ = ["process_tile"]

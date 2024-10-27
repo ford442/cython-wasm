99b42f1acf070e6c873ca722a9cbfd6bd2a96aa3
Noah Cohn <noahc42@gmail.com>
2024-10-27 01:42:51 -0700

    Update _processing.pyx

--- image_proc/_processing.pyx
+++ image_proc/_processing.pyx
@@ -5,29 +5,27 @@ from skimage import exposure, transform
 from skimage.filters import unsharp_mask
 import cv2
 
-cpdef np.ndarray[np.uint8_t, ndim=3] process_hist(np.ndarray[np.uint8_t, ndim=3] img):
-    cdef np.ndarray[np.float32_t, ndim=3] img2
-    img2 = exposure.equalize_hist(img).astype(np.float32)
-    return img2
-
 cpdef np.ndarray[np.uint8_t, ndim=3] process_tile(np.ndarray[np.uint8_t, ndim=3] tile):
     cdef int rows, cols
     cdef np.ndarray[np.float32_t, ndim=2] input_pts, output_pts,
     cdef np.ndarray[np.float64_t, ndim=2] M
     cdef np.ndarray[np.float32_t, ndim=2] N
-    cdef np.ndarray[np.float32_t, ndim=3] processed_tile
-    cdef np.ndarray[np.float32_t, ndim=3] dst, tile2,resize4x, result_1
+    cdef np.ndarray[np.float32_t, ndim=3] dst
     rows, cols = tile.shape[:2]
     input_pts = np.array([[0,0], [cols-1,0], [0,rows-1]], dtype=np.float32)
     output_pts = np.array([[cols-1,0], [0,0], [cols-1,rows-1]], dtype=np.float32)
     M = cv2.getAffineTransform(input_pts, output_pts)
     N = M.astype(np.float32)
     tile2 = tile.astype(np.float32)
-    #del tile
+    del tile
     dst = cv2.warpAffine(tile2, N, (cols,rows))
-    del tile2
-    resize4x = transform.rescale(dst, 2)
-    #del dst
+    return dst
+
+cpdef np.ndarray[np.uint8_t, ndim=3] process_tile2(np.ndarray[np.uint8_t, ndim=3] tile):
+    cdef np.ndarray[np.float32_t, ndim=3] processed_tile
+    cdef np.ndarray[np.float32_t, ndim=3] tile2, resize4x, result_1
+    resize4x = transform.rescale(tile, 2)
+    del tile
     result_1 = unsharp_mask(resize4x, radius=1, amount=1)
     del resize4x
     processed_tile = transform.pyramid_reduce(result_1, 2)
